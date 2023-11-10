4b7c8a8179e50718de924b3f59489b4a1f6500aa
Diogo Alexandre Antunes <1211165@isep.ipp.pt>
2023-11-10 11:45:34 +0000

    S5P-35 - Passageway creation

--- Visualization/src/app/PassageWay/passagewayCreate.component.ts
+++ Visualization/src/app/PassageWay/passagewayCreate.component.ts
@@ -1,115 +1,101 @@
-import {Component, inject} from '@angular/core';
-import { CommonModule } from '@angular/common';
-import {RouterLink} from "@angular/router";
-import {FloorInfoComponent} from "../Floor/floor-info/floor-info.component";
-import {HousingLocation} from "../houselocation";
-import {HousingService} from "../housing.service";
-import routes from "../routes";
+import { Component, inject } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { RouterLink } from "@angular/router";
+import { PassagewayService } from "../services/passageway.service";
 
 @Component({
-    selector: 'app-floor-create',
-    standalone: true,
-    imports: [CommonModule, RouterLink],
-    template: `
-      <section>
-          <header class="brand-name">
+  selector: "app-passageway-create",
+  standalone: true,
+  imports: [CommonModule, RouterLink],
+  template: `
+    <section>
+      <header class="brand-name">
 
-              <nav>
-                  <ul class="menuItems">
-                      <li><a [routerLink]="['/passageway']">
-                          <img class="brand-logo" src="/assets/logoPassageway.svg" alt="logo" aria-hidden="true">
-                      </a></li>
-                  </ul>
-              </nav>
-          </header>
+        <nav>
+          <ul class="menuItems">
+            <li><a [routerLink]="['/passageway']">
+              <img class="brand-logo" src="/assets/logoPassageway.svg" alt="logo" aria-hidden="true">
+            </a></li>
+          </ul>
+        </nav>
+      </header>
 
-      </section>
-      <section class="body">
-          <div class="container">
-              <div class="text">
-                  Insert Data
-              </div>
-              <form action="#">
-                  <div class="form-row">
-                      <div class="input-data">
-                          <input type="text" required>
-                          <div class="underline"></div>
-                          <label for="">Passage Code</label>
-                      </div>
-                      <div class="input-data">
-                          <input type="text" required>
-                          <div class="underline"></div>
-                          <label for="">floor1</label>
-                      </div>
-                  </div>
-                  <div class="form-row">
-                      <div class="input-data">
-                          <input type="text" required>
-                          <div class="underline"></div>
-                          <label for="">floor2</label>
-                      </div>
-
-                    </div>
-
-                  <div class="form-row">
-                      <div class="input-data textarea">
-                          <textarea rows="8" cols="80" required></textarea>
-                          <br />
-                          <div class="underline"></div>
-                          <label for="">Description</label>
-                          <br />
-                          <div class="form-row submit-btn">
-                              <div class="input-data">
-                                  <div class="inner"></div>
-                                  <a [routerLink]="['/passageway']"><input type="submit" value="submit" (click)="createBuilding()" > </a>
-                              </div>
-                          </div>
-                        </div>
-                    </div>
-              </form>
+    </section>
+    <section class="body">
+      <div class="container">
+        <div class="text">
+          Insert Data
+        </div>
+        <form action="#">
+          <div class="form-row">
+            <div class="input-data">
+              <input type="text" required>
+              <div class="underline"></div>
+              <label for="">Passage Code</label>
+            </div>
+            <div class="input-data">
+              <input type="text" required>
+              <div class="underline"></div>
+              <label for="">Floor 1</label>
+            </div>
           </div>
-          </section>
+          <div class="form-row">
+            <div class="input-data">
+              <input type="text" required>
+              <div class="underline"></div>
+              <label for="">Floor 2</label>
+            </div>
+
+          </div>
+
+          <div class="form-row">
+            <div class="input-data textarea">
+              <textarea rows="8" cols="80" required></textarea>
+              <br />
+              <div class="underline"></div>
+              <label for="">Description</label>
+              <br />
+              <div class="form-row submit-btn">
+                <div class="input-data">
+                  <div class="inner"></div>
+                  <a [routerLink]="['/passageway']"><input type="submit" value="submit" (click)="createPassageway()"> </a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </form>
+      </div>
+    </section>
   `,
-    styleUrls: ["./passagewayCreate.component.css"]
+  styleUrls: ["./passagewayCreate.component.css"]
 
 })
 
 export class PassagewayCreateComponent {
-    housingLocationList: HousingLocation[] = [];
-    housingService: HousingService = inject(HousingService);
-    filteredLocationList: HousingLocation[] = [];
+  passagewayService: PassagewayService = inject(PassagewayService);
 
-    constructor() {
-        this.housingLocationList = this.housingService.housingLocationList;
-        this.filteredLocationList = this.housingLocationList;
+  constructor() {
+  }
+
+  createPassageway() {
+    const passageCode = document.getElementsByTagName("input")[0].value;
+    const floor1 = document.getElementsByTagName("input")[1].value;
+    const floor2 = document.getElementsByTagName("input")[2].value;
+    const description = document.getElementsByTagName("textarea")[0].value;
+
+    if (passageCode == "" || floor1 == "" || floor2 == "" || description == "") {
+      alert("Please fill in all fields");
+      return;
+
     }
+    this.passagewayService.createPassageway(passageCode, floor1, floor2, description).then(() => {
 
-   createBuilding(){
+      alert("Passageway created");
 
-        const code = document.getElementsByTagName("input")[0].value;
-        const name = document.getElementsByTagName("input")[1].value;
-        const length = document.getElementsByTagName("input")[2].value;
-        const width = document.getElementsByTagName("input")[3].value;
-        const description = document.getElementsByTagName("textarea")[0].value;
+    }).catch(() => {
+      alert("Passageway not created");
+    });
 
-        if (code == "" || name == "" || length == "" || width == "" || description == ""){
-            alert("Please fill in all fields");
-            return;
-
-
-        }
-
-       if (this.housingService.createBuilding(code , name , length , width , description)){
-
-              alert("Building Created");
-
-       }else {
-              alert("Building Creation Failed");
-         }
-
-
-
-
-   }
+  }
 
 }
